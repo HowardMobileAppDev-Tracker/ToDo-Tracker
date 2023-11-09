19746fb0f4c0087289578e879bf88d607d2299b7
Shady-Rouk <olaoluwa429@gmail.com>
2023-11-09 15:30:28 -0500

    Set up starter code for each page of the app.

---
+++ app/src/main/java/com/appdev/to_do_tracker/AddItemActivity.kt
@@ -0,0 +1,11 @@
+package com.appdev.to_do_tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item)
+    }
+}
