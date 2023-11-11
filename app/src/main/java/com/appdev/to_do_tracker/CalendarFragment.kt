873ceafb48f4f053ea57d5a3584f045fbf83fa4f
Shady-Rouk <olaoluwa429@gmail.com>
2023-11-11 10:47:13 -0500

    Set up calendar filter frontend and backend.

--- app/src/main/java/com/appdev/to_do_tracker/CalendarFragment.kt
+++ app/src/main/java/com/appdev/to_do_tracker/CalendarFragment.kt
@@ -1,33 +1,26 @@
 package com.appdev.to_do_tracker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CalendarView
+import java.util.Calendar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
- * A simple [Fragment] subclass.
- * Use the [CalendarFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A Fragment class for the Calendar Filter view of the app.
  */
 class CalendarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val currentDate: Calendar = Calendar.getInstance()
+    private lateinit var calInputView: CalendarView
+    private lateinit var filterButton: Button
+    private var filterDay: Int = currentDate.get(Calendar.DAY_OF_MONTH)
+    private var filterMonth: Int = currentDate.get(Calendar.MONTH) + 1
+    private var filterYear: Int = currentDate.get(Calendar.YEAR)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +30,22 @@ class CalendarFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalendarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalendarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        calInputView = view.findViewById(R.id.calFilter)
+        filterButton = view.findViewById(R.id.filterSubmitBtn)
+
+        calInputView.setOnDateChangeListener { calView, year, month, dayOfMonth ->
+            filterDay = dayOfMonth
+            filterMonth = month+1
+            filterYear = year
+        }
+
+        filterButton.setOnClickListener {
+            // pull necessary records from database and pass to Home, or pass date to Home and pull in Home.
+            Log.v("Calendar Listener", "Chosen date is $filterMonth/$filterDay/$filterYear")
+        }
     }
+
 }
--- app/src/main/java/com/appdev/to_do_tracker/MainActivity.kt
+++ app/src/main/java/com/appdev/to_do_tracker/MainActivity.kt
@@ -2,10 +2,37 @@ package com.appdev.to_do_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pastDueFrag: Fragment = PastDueFragment()
+        val homeFrag: Fragment = HomeFragment()
+        val calendarFilterFrag: Fragment = CalendarFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.nav_late -> fragment = pastDueFrag
+                R.id.nav_home -> fragment = homeFrag
+                R.id.nav_calendar -> fragment = calendarFilterFrag
+            }
+            replaceFragment(fragment)
+            true
+        }
+        bottomNavigationView.selectedItemId = R.id.nav_home
+    }
+
+    private fun replaceFragment(newFragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frame_layout, newFragment)
+        fragmentTransaction.commit()
     }
 }
