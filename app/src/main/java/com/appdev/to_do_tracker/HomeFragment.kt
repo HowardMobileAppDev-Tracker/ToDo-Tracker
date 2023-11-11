63bf666ad7ac98af7392ddb374ce8327775c5c59
Shady-Rouk <olaoluwa429@gmail.com>
2023-11-11 17:44:01 -0500

    Add code to pull records from the local database based on the date filter.

--- app/src/main/java/com/appdev/to_do_tracker/HomeFragment.kt
+++ app/src/main/java/com/appdev/to_do_tracker/HomeFragment.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class HomeFragment(private val todoRecords: List<ToDoRecord>) : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,23 +37,4 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/appdev/to_do_tracker/MainActivity.kt
+++ app/src/main/java/com/appdev/to_do_tracker/MainActivity.kt
@@ -4,25 +4,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.launch
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     private val currentDate: Calendar = Calendar.getInstance()
+    private val todoRecords = mutableListOf<ToDoRecord>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Date to pull data to show in the Home page. Set to Today's date by default. Reassigned
         // from Calendar view.
-        var filterDay = intent.getIntExtra("DayToShow", currentDate.get(Calendar.DAY_OF_MONTH))
-        var filterMonth = intent.getIntExtra("MonthToShow", currentDate.get(Calendar.MONTH)+1)
-        var filterYear = intent.getIntExtra("YearToShow", currentDate.get(Calendar.YEAR))
+        val filterDay = intent.getIntExtra("DayToShow", currentDate.get(Calendar.DAY_OF_MONTH))
+        val filterMonth = intent.getIntExtra("MonthToShow", currentDate.get(Calendar.MONTH)+1)
+        val filterYear = intent.getIntExtra("YearToShow", currentDate.get(Calendar.YEAR))
 
         // Log.v("MainActivityTag", "Chosen date is $filterMonth/$filterDay/$filterYear")
 
+        lifecycleScope.launch {
+            (application as ToDoApplication).db.todoDao().getRecordsByDate(filterDay, filterMonth, filterYear).collect { databaseList ->
+                databaseList.map { entity ->
+                    ToDoRecord(
+                        entity.todoTitle,
+                        entity.isComplete,
+                        entity.deadlineDay,
+                        entity.deadlineMonth,
+                        entity.deadlineYear,
+                        entity.shouldRemind
+                    )
+                }.also { mappedList ->
+                    todoRecords.clear()
+                    todoRecords.addAll(mappedList)
+                }
+            }
+        }
+
         val pastDueFrag: Fragment = PastDueFragment()
-        val homeFrag: Fragment = HomeFragment()
+        // todoRecords is passed into homeFrag so homeFrag can handle displaying each ToDoRecord object.
+        val homeFrag: Fragment = HomeFragment(todoRecords)
         val calendarFilterFrag: Fragment = CalendarFragment()
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
