77ea97287b09f456f7ddc1eb145bbc381efe325e
Shady-Rouk <olaoluwa429@gmail.com>
2023-11-20 22:40:40 -0500

    Show the date associated with tasks on the Home page.

--- app/src/main/java/com/appdev/to_do_tracker/HomeFragment.kt
+++ app/src/main/java/com/appdev/to_do_tracker/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,10 +22,12 @@ class HomeFragment(private val activity: MainActivity, private val filterDay: In
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_item_list, container, false)
-        val recyclerView = view.findViewById(R.id.itemRv) as RecyclerView
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val recyclerView = view.findViewById(R.id.itemRvHome) as RecyclerView
         val adapter = ItemListAdapter(activity, todoRecords)
         val context = view.context
+        val selectedDateView = view.findViewById<TextView>(R.id.selectedDate)
+        selectedDateView.text = "$filterMonth/$filterDay/$filterYear"
         val priorityOrder = mapOf("High" to 0, "Medium" to 1, "Low" to 2)
 
         lifecycleScope.launch {
